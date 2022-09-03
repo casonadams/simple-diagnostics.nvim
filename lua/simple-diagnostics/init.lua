53ca85eaf3611b1cb0c08f7d566f3107e7132367
Cason Adams <casonadams@gmail.com>
2022-09-03 03:17:25 -0600

    fix: clear on non file buffers error

--- lua/simple-diagnostics/init.lua
+++ lua/simple-diagnostics/init.lua
@@ -1,5 +1,13 @@
+local set = false
 local show_virtual_text = true
 local show_message_area = true
+
+local severity = {
+  [1] = "LspDiagnosticsVirtualTextError",
+  [2] = "LspDiagnosticsVirtualTextWarning",
+  [3] = "LspDiagnosticsVirtualTextInformation",
+  [4] = "LspDiagnosticsVirtualTextHint",
+}
 
 local function setup(parameters)
   if parameters.show_message_area == nil then
@@ -15,30 +23,32 @@ local function setup(parameters)
   end
 end
 
-local function PrintDiagnostics(opts, bufnr, line_nr, client_id)
-  opts = opts or {}
-  bufnr = bufnr or 0
-  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+local function clear()
+  if not set then return end
+
+  local bufnr = 0
+  if show_message_area then
+    print(" ")
+  end
+  if show_virtual_text then
+    vim.api.nvim_buf_clear_namespace(bufnr, -1, 0, -1)
+  end
+  set = false
+end
+
+local function printDiagnostics(line_nr)
+  local bufnr = 0
+  local line_diagnostics = vim.lsp.diagnostic.get_line_diagnostics(bufnr, line_nr)
+
+  if vim.tbl_isempty(line_diagnostics) then
+    clear()
+    return
+  end
+
+  if set then return end
 
   local diagnostic_message = ""
   local diagnostic_severity = ""
-  local severity = {
-    [1] = "LspDiagnosticsVirtualTextError",
-    [2] = "LspDiagnosticsVirtualTextWarning",
-    [3] = "LspDiagnosticsVirtualTextInformation",
-    [4] = "LspDiagnosticsVirtualTextHint",
-  }
-
-  local line_diagnostics = vim.lsp.diagnostic.get_line_diagnostics(
-    bufnr,
-    line_nr,
-    opts,
-    client_id
-  )
-
-  if vim.tbl_isempty(line_diagnostics) then
-    return
-  end
 
   for _, diagnostic in ipairs(line_diagnostics) do
     diagnostic_message = diagnostic_message ..
@@ -49,39 +59,27 @@ local function PrintDiagnostics(opts, bufnr, line_nr, client_id)
   end
 
   if show_message_area then
-    print(diagnostic_message) -- prints to MsgArea
+    print(diagnostic_message)
   end
 
   if show_virtual_text then
     vim.api.nvim_buf_set_virtual_text(bufnr, 1, line_nr, { { diagnostic_message, severity[diagnostic_severity] } }, {})
   end
-end
-
-local function Clear(opts, bufnr, line_nr)
-  opts = opts or {}
-  bufnr = bufnr or 0
-  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-
-  if show_message_area then
-    print("      ") -- print to MsgArea
-  end
-  if show_virtual_text then
-    vim.api.nvim_buf_clear_namespace(bufnr, -1, 0, -1)
-  end
+  set = true
 end
 
 local augroup = vim.api.nvim_create_augroup('simple-diagnostics', { clear = true })
 
-vim.api.nvim_create_autocmd({ 'CursorHold' }, {
-  pattern = '*',
+vim.api.nvim_create_autocmd({ 'CursorHold', 'InsertLeave' }, {
+  pattern = '*.*',
   group = augroup,
-  callback = function() PrintDiagnostics() end
+  callback = function() printDiagnostics(vim.api.nvim_win_get_cursor(0)[1] - 1) end
 })
 
-vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
-  pattern = '*',
+vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+  pattern = '*.*',
   group = augroup,
-  callback = function() Clear() end
+  callback = function() clear() end
 })
 
 return {

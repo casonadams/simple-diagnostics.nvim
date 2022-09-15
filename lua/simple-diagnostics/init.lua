e99231aa24decf6361b36e3704502f7f50a40afc
Cason Adams <casonadams@gmail.com>
2022-09-15 00:01:01 -0600

    fix: errors with split buffers

--- lua/simple-diagnostics/init.lua
+++ lua/simple-diagnostics/init.lua
@@ -1,7 +1,8 @@
 local set = false
 local show_virtual_text = true
 local show_message_area = true
-local prev_line_nr = 0
+local prev_line_nr = vim.api.nvim_win_get_cursor(0)[1]
+local prev_bufnr = vim.api.nvim_get_current_buf()
 
 local severity = {
   [1] = "LspDiagnosticsVirtualTextError",
@@ -25,32 +26,29 @@ local function setup(parameters)
   end
 end
 
-local function clear()
+local function clear(bufnr)
   if not set then return end
+  if prev_bufnr ~= bufnr then return end
 
-  local bufnr = 0
   if show_message_area then
     print(" ")
   end
   if show_virtual_text then
     local opts = {
-      -- end_line = 10,
       id = 1,
       virt_text = { { "", "" } },
       virt_text_pos = 'eol',
-      -- virt_text_win_col = 20,
     }
     vim.api.nvim_buf_set_extmark(bufnr, 1, prev_line_nr, 10, opts)
   end
   set = false
 end
 
-local function printDiagnostics(line_nr)
-  local bufnr = 0
+local function printDiagnostics(bufnr, line_nr)
   local line_diagnostics = vim.lsp.diagnostic.get_line_diagnostics(bufnr, line_nr)
 
   if vim.tbl_isempty(line_diagnostics) then
-    clear()
+    clear(bufnr)
     return
   end
 
@@ -86,13 +84,19 @@ local augroup = vim.api.nvim_create_augroup('simple-diagnostics', { clear = true
 vim.api.nvim_create_autocmd({ 'CursorHold', 'InsertLeave' }, {
   pattern = '*.*',
   group = augroup,
-  callback = function() printDiagnostics(vim.api.nvim_win_get_cursor(0)[1] - 1) end
+  callback = function() printDiagnostics(
+      vim.api.nvim_get_current_buf(),
+      vim.api.nvim_win_get_cursor(0)[1] - 1)
+  end
 })
 
 vim.api.nvim_create_autocmd({ 'CursorMoved', 'InsertEnter', 'BufEnter' }, {
   pattern = '*.*',
   group = augroup,
-  callback = function() clear() end
+  callback = function() clear(
+      vim.api.nvim_get_current_buf()
+    )
+  end
 })
 
 return {
